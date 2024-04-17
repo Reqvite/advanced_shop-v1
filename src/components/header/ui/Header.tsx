72136a075a871766997fb0585bceb22682a8ac5c
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-17 17:10:55 +0300

    update package.json

--- src/components/header/ui/Header.tsx
+++ src/components/header/ui/Header.tsx
@@ -1,11 +1,11 @@
 import {AppBar} from '@mui/material';
 import {ReactElement} from 'react';
-import {Navbar} from './Navbar';
+import {AuthButton} from '@/shared/ui';
 
 export const Header = (): ReactElement => {
   return (
     <AppBar position="static">
-      <Navbar />
+      <AuthButton />
     </AppBar>
   );
 };
