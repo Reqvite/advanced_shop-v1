d219037999c132c6674d5ada1e988a04b79a3cc9
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-24 10:57:48 +0300

    fix deploy error

--- src/shared/ui/button/AddToCartButton.tsx
+++ src/shared/ui/button/AddToCartButton.tsx
@@ -1,10 +1,11 @@
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import {ButtonProps} from '@mui/material';
+import {ReactElement} from 'react';
 import {Button} from './Button';
 
 type Props = ButtonProps;
 
-export const AddToCartButton = (props: Props) => {
+export const AddToCartButton = (props: Props): ReactElement => {
   return (
     <Button
       variant="contained"
--- src/shared/ui/button/WishlistButton.tsx
+++ src/shared/ui/button/WishlistButton.tsx
@@ -1,10 +1,11 @@
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import {ButtonProps} from '@mui/material';
+import {ReactElement} from 'react';
 import {Button} from './Button';
 
 type Props = ButtonProps;
 
-export const WishlistButton = (props: Props) => {
+export const WishlistButton = (props: Props): ReactElement => {
   return (
     <Button
       variant="outlined"
