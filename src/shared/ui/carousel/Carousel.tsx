51b77d60017fd08db0b65970d46f8d7da0e9cbf2
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-05-02 18:45:25 +0300

    add carousel

---
+++ src/shared/ui/carousel/Carousel.tsx
@@ -0,0 +1,37 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import {Box} from '@mui/material';
+import {ComponentType} from 'react';
+import {Swiper, SwiperProps, SwiperSlide} from 'swiper/react';
+import {carouselStyles} from '@/app/theme/styles';
+import {Flex} from '../base/Flex';
+
+interface ItemWithId {
+  _id: string;
+}
+
+type Props<T extends ItemWithId> = SwiperProps & {
+  items: T[];
+  component: ComponentType<any>;
+};
+
+export const Carousel = <T extends ItemWithId>({
+  items,
+  component: Component,
+  ...otherProps
+}: Props<T>) => {
+  return (
+    <Box>
+      <Swiper style={carouselStyles.swiper} {...otherProps}>
+        <Flex gap={1}>
+          {items.map((item) => (
+            <SwiperSlide style={carouselStyles.swiperSlide} key={item._id}>
+              <Component {...item} />
+            </SwiperSlide>
+          ))}
+        </Flex>
+      </Swiper>
+    </Box>
+  );
+};
--- src/shared/ui/product/RecommendedProductList.tsx
+++ src/shared/ui/product/RecommendedProductList.tsx
@@ -1,12 +1,12 @@
 import {Stack, StackProps, Typography} from '@mui/material';
 import {ReactElement} from 'react';
+import {Autoplay} from 'swiper/modules';
 import {getRouteMain} from '@/app/providers/AppRouter/routeConfig';
 import {ProductI} from '@/shared/types/product';
 import {Flex} from '../base/Flex';
 import {NavigateButton} from '../button/NavigateButton';
-import {List} from '../list/List';
+import {Carousel} from '../carousel/Carousel';
 import {ProductCard} from './ProductCard';
-import {ProductCardSkeleton} from './ProductCard.skeleton';
 
 type Props = StackProps & {
   products: ProductI[];
@@ -19,7 +19,7 @@ export const RecommendedProductList = ({
   ...otherProps
 }: Props): ReactElement => {
   const renderProductCard = (product: ProductI) => {
-    return <ProductCard variant="small" {...product} />;
+    return <ProductCard variant="small" sx={{height: 350}} {...product} />;
   };
 
   return (
@@ -28,15 +28,30 @@ export const RecommendedProductList = ({
         <Typography variant="h5">You will maybe love</Typography>
         <NavigateButton label="More products" to={getRouteMain()} />
       </Flex>
-      <List
-        items={products?.slice(0, 4)}
-        row
-        renderItem={renderProductCard}
-        isLoading={isLoading}
-        skeleton={<ProductCardSkeleton variant="small" />}
-        skeletonLength={4}
-        itemStyle={{width: 'auto'}}
-      />
+      {!isLoading && (
+        <Carousel
+          items={products}
+          // autoplay
+          slidesPerView={1}
+          loop
+          modules={[Autoplay]}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 30
+            },
+            1000: {
+              slidesPerView: 3,
+              spaceBetween: 30
+            },
+            1260: {
+              slidesPerView: 4,
+              spaceBetween: 30
+            }
+          }}
+          component={renderProductCard}
+        />
+      )}
     </Stack>
   );
 };
