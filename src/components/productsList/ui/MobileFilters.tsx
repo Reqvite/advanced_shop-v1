43b8a5c00066164ab791e1b12fe52670444347a9
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-05-15 15:30:13 +0300

    delete unused import

--- src/components/productsList/ui/MobileFilters.tsx
+++ src/components/productsList/ui/MobileFilters.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {ReactElement} from 'react';
 import {FieldValues} from 'react-hook-form';
 import {productsListStyles} from '@/app/theme/styles';
