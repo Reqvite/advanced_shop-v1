de1821a283e1454a3a52e718b6d39aa82dfc066d
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-06-03 10:46:52 +0300

    added getBillingInfo

--- src/shared/ui/order/OrderCard.tsx
+++ src/shared/ui/order/OrderCard.tsx
@@ -1,16 +1,20 @@
 import {Card} from '@mui/material';
 import {ReactElement} from 'react';
 import {formateDate} from '@/shared/lib/helpers';
+import {LabelOptionsI} from '@/shared/types/options';
 import {OrderI} from '@/shared/types/order';
 import {Title} from '../base/Title';
 
 type Props = OrderI;
 
-export const OrderCard = ({_id, createdAt, billingInfo}: Props): ReactElement => {
-  const deliveryInformation = billingInfo
+const getBillingInfo = (billingInfo: LabelOptionsI[]): string =>
+  billingInfo
     .map((el) => el.value)
     .join(', ')
     .slice(0, -2);
+
+export const OrderCard = ({_id, createdAt, billingInfo}: Props): ReactElement => {
+  const deliveryInformation = getBillingInfo(billingInfo);
 
   return (
     <Card sx={{width: '100%', padding: 3, maxWidth: 1000}}>
