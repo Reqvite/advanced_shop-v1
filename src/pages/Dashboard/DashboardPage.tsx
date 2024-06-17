2965f890a12615697d34aabc6a45999158a988a1
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-06-17 10:27:12 +0300

    updated

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -1,13 +1,8 @@
 import {ReactElement} from 'react';
 import {PageWrapper} from '@/shared/ui';
-import {Bar} from '@/shared/ui/charts';
 
 const DashboardPage = (): ReactElement => {
-  return (
-    <PageWrapper>
-      <Bar />
-    </PageWrapper>
-  );
+  return <PageWrapper>{/* <Bar /> */}</PageWrapper>;
 };
 
 export default DashboardPage;
