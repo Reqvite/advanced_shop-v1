6b57d3a0c94163ec913af598436d767aa8fc5baa
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-06-21 12:31:30 +0300

    updated dashboard page

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -2,10 +2,15 @@ import {Stack} from '@mui/material';
 import {ReactElement} from 'react';
 import {BarTimelineCard, ChoroplethCard, PieCard} from '@/components/charts';
 import {timelineOptions} from '@/shared/lib/helpers/enumLabelResolver/options';
+import {useMediaQuery} from '@/shared/lib/hooks';
 import {Flex, PageWrapper} from '@/shared/ui';
 import {useGetOrdersStatisticQuery} from '@/slices/dashboard/dashboard.rtk';
 
 const DashboardPage = (): ReactElement => {
+  const isMobile = useMediaQuery('md');
+  const width = isMobile ? '100%' : '50%';
+  const flexWrap = isMobile ? 'wrap' : 'nowrap';
+
   return (
     <PageWrapper withScroll={false}>
       <Stack gap={2}>
@@ -14,14 +19,14 @@ const DashboardPage = (): ReactElement => {
           title="Earnings by"
           useQuery={useGetOrdersStatisticQuery}
         />
-        <Flex width="100%" gap={2}>
+        <Flex width="100%" gap={2} flexWrap={flexWrap}>
           <PieCard
             tabOptions={timelineOptions}
             title="Earnings by"
             useQuery={useGetOrdersStatisticQuery}
-            sx={{width: '50%'}}
+            sx={{width}}
           />
-          <ChoroplethCard title="Orders Distribution by Country Map" sx={{width: '50%'}} />
+          <ChoroplethCard title="Orders Distribution by Country Map" sx={{width}} />
         </Flex>
       </Stack>
     </PageWrapper>
